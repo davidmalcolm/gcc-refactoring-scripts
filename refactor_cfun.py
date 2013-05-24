fc1881c81e8c6899f33b4c0e3aa087e4021e9a18
David Malcolm <dmalcolm@redhat.com>
2013-05-24 18:56:31 -0400

    add script for making implicit uses of cfun explicit

---
+++ refactor_cfun.py
@@ -0,0 +1,48 @@
+import re
+
+from refactor import main
+
+def expand_cfun_macros(filename, src):
+    if filename == 'basic-block.h':
+        return src, ''
+
+    prev_not_ident = '(?<=[^_0-9a-zA-Z])'
+    succ_not_ident = '(?=[^_0-9a-zA-Z])'
+    rules = [('ENTRY_BLOCK_PTR(^_FOR)', 'cfun->cfg->get_entry_block ()'),
+             ('EXIT_BLOCK_PTR(^_FOR)', 'cfun->cfg->get_exit_block ()'),
+             (prev_not_ident + 'basic_block_info' + succ_not_ident,
+              'cfun->cfg->get_basic_block_info ()'),
+             (prev_not_ident + 'n_basic_blocks' + succ_not_ident,
+              'cfun->cfg->get_n_basic_blocks ()'),
+             (prev_not_ident + 'n_edges' + succ_not_ident,
+              'cfun->cfg->get_n_edges ()'),
+             (prev_not_ident + 'last_basic_block' + succ_not_ident,
+              'cfun->cfg->get_last_basic_block ()'),
+             (prev_not_ident + 'label_to_block_map' + succ_not_ident,
+              'cfun->cfg->get_label_to_block_map ()'),
+             (prev_not_ident + 'profile_status' + succ_not_ident,
+              'cfun->cfg->get_profile_status ()'),
+             (prev_not_ident + 'BASIC_BLOCK \((?P<N>.+)\)',
+              'cfun->cfg.get_basic_block_by_idx (%(N)s)'),
+             (prev_not_ident + 'SET_BASIC_BLOCK \((?P<N>.+), (?P<BB>.+)\)',
+              'cfun->cfg.set_basic_block_by_idx (%(N)s, %(BB)s)'),
+             ('FOR_EACH_BB \((?P<BB>.+)\)',
+              'FOR_EACH_BB_CFG (%(BB)s, cfun->cfg)'),
+             ]
+    while 1:
+        match = 0
+        for pat_in, pat_out in rules:
+            m = re.search(pat_in, src)
+            if m:
+                replacement = pat_out % m.groupdict()
+                # print(replacement)
+                src = (src[:m.start()] + replacement + src[m.end():])
+                match = 1
+        if not match:
+            break
+
+    #print(src)
+    return src, '' # FIXME: changelog entry
+
+if __name__ == '__main__':
+    main('refactor_cfun.py', expand_cfun_macros)
---
+++ test_refactor_cfun.py
@@ -0,0 +1,69 @@
+from refactor_cfun import expand_cfun_macros
+import unittest
+
+class MacroTests(unittest.TestCase):
+    def assertRefactoringEquals(self,
+                                src, filename,
+                                expected_code, expected_changelog):
+        actual_code, actual_changelog = expand_cfun_macros(filename, src)
+        self.maxDiff = 8192
+        self.assertMultiLineEqual(expected_code, actual_code) # 2.7+
+        self.assertMultiLineEqual(expected_changelog, actual_changelog) # 2.7+
+    def assertUnchanged(self, src, filename):
+        self.assertRefactoringEquals(src, filename, src, '')
+
+    def test_n_basic_blocks(self):
+        src = (
+            'rpo = XNEWVEC (int, n_basic_blocks);')
+        expected_code = (
+            'rpo = XNEWVEC (int, cfun->cfg->get_n_basic_blocks ());')
+        expected_changelog = ''
+        self.assertRefactoringEquals(src, 'alias.c',
+                                     expected_code, expected_changelog)
+
+    def test_FOR_EACH_BB(self):
+        src = (
+            '  int saved_last_basic_block = last_basic_block;\n'
+            '\n'
+            '  FOR_EACH_BB (bb)\n'
+            '     {\n'
+            '       basic_block prev_bb = bb;\n')
+        expected_code = (
+            '  int saved_last_basic_block = cfun->cfg->get_last_basic_block ();\n'
+            '\n'
+            '  FOR_EACH_BB_CFG (bb, cfun->cfg)\n'
+            '     {\n'
+            '       basic_block prev_bb = bb;\n')
+        expected_changelog = ''
+        self.assertRefactoringEquals(src, 'asan.c',
+                                     expected_code, expected_changelog)
+
+    def test_BASIC_BLOCK(self):
+        src = (
+            'basic_block bb = BASIC_BLOCK (rpo[i]);')
+        expected_code = (
+            'basic_block bb = cfun->cfg.get_basic_block_by_idx (rpo[i]);')
+        expected_changelog = ''
+        self.assertRefactoringEquals(src, 'alias.c',
+                                     expected_code, expected_changelog)
+
+    def test_NOTE_BASIC_BLOCK(self):
+        self.assertUnchanged('NOTE_BASIC_BLOCK (note) = bb;', 'cfgexpand.c')
+
+    def test_profile_status(self):
+        src = (
+            '  if (profile_status != PROFILE_ABSENT)\n')
+        expected_code = (
+            '  if (cfun->cfg->get_profile_status () != PROFILE_ABSENT)\n')
+        expected_changelog = ''
+        self.assertRefactoringEquals(src, 'cfgbuild.c',
+                                     expected_code, expected_changelog)
+
+
+    def test_profile_status_for_function(self):
+        self.assertUnchanged(
+            '  if (profile_status_for_function (fun) == PROFILE_ABSENT)',
+            'predict.c')
+
+if __name__ == '__main__':
+    unittest.main()
