9beb9d13f9daf5a3d1c825cda7950d4f52538cad
David Malcolm <dmalcolm@redhat.com>
2013-05-28 15:22:37 -0400

    introduce Macro class

--- refactor_cfun.py
+++ refactor_cfun.py
@@ -1,7 +1,60 @@
-from collections import OrderedDict
+from collections import namedtuple, OrderedDict
 import re
 
 from refactor import main, Changelog, get_change_scope, within_comment
+
+class Macro(namedtuple("macro", ("name", "pattern", "expansion"))):
+    pass
+
+prev_not_ident = '(?<=[^_0-9a-zA-Z])'
+succ_not_ident = '(?=[^_0-9a-zA-Z])'
+macros = \
+    [Macro(name, pattern, expansion)
+     for name, pattern, expansion in [
+        # basic-block.h:
+        ('ENTRY_BLOCK_PTR',
+         'ENTRY_BLOCK_PTR' + succ_not_ident,
+         'cfun->cfg->get_entry_block ()'),
+        ('EXIT_BLOCK_PTR',
+         'EXIT_BLOCK_PTR' + succ_not_ident,
+         'cfun->cfg->get_exit_block ()'),
+        ('basic_block_info',
+         prev_not_ident + 'basic_block_info' + succ_not_ident,
+         'cfun->cfg->get_basic_block_info ()'),
+        ('n_basic_blocks',
+         prev_not_ident + 'n_basic_blocks' + succ_not_ident,
+         'cfun->cfg->get_n_basic_blocks ()'),
+        ('n_edges',
+         prev_not_ident + 'n_edges' + succ_not_ident,
+         'cfun->cfg->get_n_edges ()'),
+        ('last_basic_block',
+         prev_not_ident + 'last_basic_block' + succ_not_ident,
+         'cfun->cfg->get_last_basic_block ()'),
+        ('label_to_block_map',
+         prev_not_ident + 'label_to_block_map' + succ_not_ident,
+         'cfun->cfg->get_label_to_block_map ()'),
+        ('profile_status',
+         prev_not_ident + 'profile_status = (?P<ENUM_VALUE>PROFILE_[A-Z]+);',
+         'cfun->cfg->set_profile_status (%(ENUM_VALUE)s);'),
+        ('profile_status',
+         prev_not_ident + 'profile_status' + succ_not_ident,
+         'cfun->cfg->get_profile_status ()'),
+        ('BASIC_BLOCK',
+         prev_not_ident + 'BASIC_BLOCK \((?P<N>.+)\)',
+         'cfun->cfg->get_basic_block_by_idx (%(N)s)'),
+        ('SET_BASIC_BLOCK',
+         prev_not_ident + 'SET_BASIC_BLOCK \((?P<N>.+), (?P<BB>.+)\)',
+         'cfun->cfg->set_basic_block_by_idx (%(N)s, %(BB)s)'),
+        ('FOR_EACH_BB',
+         'FOR_EACH_BB \((?P<BB>.+)\)',
+         'FOR_EACH_BB_CFG (%(BB)s, cfun->cfg)'),
+        ('FOR_ALL_BB',
+         'FOR_ALL_BB ?\((?P<BB>.+)\)',
+         'FOR_ALL_BB_CFG (%(BB)s, cfun->cfg)'),
+        ('FOR_EACH_BB_REVERSE',
+         'FOR_EACH_BB_REVERSE \((?P<BB>.+)\)',
+         'FOR_EACH_BB_REVERSE_CFG (%(BB)s, cfun->cfg)'),
+        ]]
 
 def expand_cfun_macros(filename, src):
     if filename in ('basic-block.h',
@@ -9,67 +62,22 @@ def expand_cfun_macros(filename, src):
                     ):
         return src, ''
 
-    prev_not_ident = '(?<=[^_0-9a-zA-Z])'
-    succ_not_ident = '(?=[^_0-9a-zA-Z])'
-    rules = [('ENTRY_BLOCK_PTR',
-              'ENTRY_BLOCK_PTR' + succ_not_ident,
-              'cfun->cfg->get_entry_block ()'),
-             ('EXIT_BLOCK_PTR',
-              'EXIT_BLOCK_PTR' + succ_not_ident,
-              'cfun->cfg->get_exit_block ()'),
-             ('basic_block_info',
-              prev_not_ident + 'basic_block_info' + succ_not_ident,
-              'cfun->cfg->get_basic_block_info ()'),
-             ('n_basic_blocks',
-              prev_not_ident + 'n_basic_blocks' + succ_not_ident,
-              'cfun->cfg->get_n_basic_blocks ()'),
-             ('n_edges',
-              prev_not_ident + 'n_edges' + succ_not_ident,
-              'cfun->cfg->get_n_edges ()'),
-             ('last_basic_block',
-              prev_not_ident + 'last_basic_block' + succ_not_ident,
-              'cfun->cfg->get_last_basic_block ()'),
-             ('label_to_block_map',
-              prev_not_ident + 'label_to_block_map' + succ_not_ident,
-              'cfun->cfg->get_label_to_block_map ()'),
-             ('profile_status',
-              prev_not_ident + 'profile_status = (?P<ENUM_VALUE>PROFILE_[A-Z]+);',
-              'cfun->cfg->set_profile_status (%(ENUM_VALUE)s);'),
-             ('profile_status',
-              prev_not_ident + 'profile_status' + succ_not_ident,
-              'cfun->cfg->get_profile_status ()'),
-             ('BASIC_BLOCK',
-              prev_not_ident + 'BASIC_BLOCK \((?P<N>.+)\)',
-              'cfun->cfg->get_basic_block_by_idx (%(N)s)'),
-             ('SET_BASIC_BLOCK',
-              prev_not_ident + 'SET_BASIC_BLOCK \((?P<N>.+), (?P<BB>.+)\)',
-              'cfun->cfg->set_basic_block_by_idx (%(N)s, %(BB)s)'),
-             ('FOR_EACH_BB',
-              'FOR_EACH_BB \((?P<BB>.+)\)',
-              'FOR_EACH_BB_CFG (%(BB)s, cfun->cfg)'),
-             ('FOR_ALL_BB',
-              'FOR_ALL_BB ?\((?P<BB>.+)\)',
-              'FOR_ALL_BB_CFG (%(BB)s, cfun->cfg)'),
-             ('FOR_EACH_BB_REVERSE',
-              'FOR_EACH_BB_REVERSE \((?P<BB>.+)\)',
-              'FOR_EACH_BB_REVERSE_CFG (%(BB)s, cfun->cfg)')
-             ]
     changelog = Changelog(filename)
     macros_removed_by_scope = OrderedDict()
     while 1:
         match = 0
-        for macro, pat_in, pat_out in rules:
-            for m in re.finditer(pat_in, src):
-                replacement = pat_out % m.groupdict()
+        for macro in macros:
+            for m in re.finditer(macro.pattern, src):
+                replacement = macro.expansion % m.groupdict()
                 # print(replacement)
                 if within_comment(src, m.start()):
                     continue
                 src = (src[:m.start()] + replacement + src[m.end():])
                 scope = get_change_scope(src, m.start())
                 if scope in macros_removed_by_scope:
-                    macros_removed_by_scope[scope].add(macro)
+                    macros_removed_by_scope[scope].add(macro.name)
                 else:
-                    macros_removed_by_scope[scope] = set([macro])
+                    macros_removed_by_scope[scope] = set([macro.name])
 
                 # only process one match at most per re.finditer,
                 # since the m.start/end will no longer correspond
@@ -79,13 +87,13 @@ def expand_cfun_macros(filename, src):
         if not match:
             break
     for scope in macros_removed_by_scope:
-        macros = sorted(macros_removed_by_scope[scope])
-        if len(macros) == 1:
+        macro_names = sorted(macros_removed_by_scope[scope])
+        if len(macro_names) == 1:
             changelog.append('(%s): Remove usage of %s macro.\n'
-                             % (scope, macros[0]))
+                             % (scope, macro_names[0]))
         else:
             changelog.append('(%s): Remove uses of macros: %s.\n'
-                             % (scope, ', '.join(macros)))
+                             % (scope, ', '.join(macro_names)))
 
 
     #print(src)
