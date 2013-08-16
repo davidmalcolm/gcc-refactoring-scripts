2754791c1e9183037e111e7937836ec2ef031c44
David Malcolm <dmalcolm@redhat.com>
2013-08-16 18:46:25 -0400

    add another test case

--- test_refactor_ipa_passes.py
+++ test_refactor_ipa_passes.py
@@ -13,7 +13,7 @@ class IpaPassConversionTests(unittest.TestCase):
         self.assertMultiLineEqual(expected_changelog,
                                   actual_changelog.as_text(None)[0]) # 2.7+
 
-    def test_pass_(self):
+    def test_ipa_reference(self):
         src = """
 namespace {
 
@@ -121,6 +121,121 @@ public:
              '\tpasses.\n')
 
         self.assertRefactoringEquals(src, 'ipa-reference.c',
+                                     expected_code, expected_changelog)
+
+    def test_ipa_pure_const(self):
+        src = (
+"""
+namespace {
+
+const pass_data pass_data_ipa_pure_const =
+{
+  IPA_PASS, /* type */
+  "pure-const", /* name */
+  OPTGROUP_NONE, /* optinfo_flags */
+  true, /* has_gate */
+  true, /* has_execute */
+  TV_IPA_PURE_CONST, /* tv_id */
+  0, /* properties_required */
+  0, /* properties_provided */
+  0, /* properties_destroyed */
+  0, /* todo_flags_start */
+  0, /* todo_flags_finish */
+};
+
+class pass_ipa_pure_const : public ipa_opt_pass_d
+{
+public:
+  pass_ipa_pure_const(gcc::context *ctxt)
+    : ipa_opt_pass_d(pass_data_ipa_pure_const, ctxt,
+\t\t     pure_const_generate_summary, /* generate_summary */
+\t\t     pure_const_write_summary, /* write_summary */
+\t\t     pure_const_read_summary, /* read_summary */
+\t\t     NULL, /* write_optimization_summary */
+\t\t     NULL, /* read_optimization_summary */
+\t\t     NULL, /* stmt_fixup */
+\t\t     0, /* function_transform_todo_flags_start */
+\t\t     NULL, /* function_transform */
+\t\t     NULL) /* variable_transform */
+  {}
+
+  /* opt_pass methods: */
+  bool gate () { return gate_pure_const (); }
+  unsigned int execute () { return propagate (); }
+
+}; // class pass_ipa_pure_const
+
+} // anon namespace
+""")
+        expected_code = (
+"""
+namespace {
+
+const pass_data pass_data_ipa_pure_const =
+{
+  IPA_PASS, /* type */
+  "pure-const", /* name */
+  OPTGROUP_NONE, /* optinfo_flags */
+  true, /* has_gate */
+  true, /* has_execute */
+  TV_IPA_PURE_CONST, /* tv_id */
+  0, /* properties_required */
+  0, /* properties_provided */
+  0, /* properties_destroyed */
+  0, /* todo_flags_start */
+  0, /* todo_flags_finish */
+};
+
+const ipa_pass_data ipa_pass_data_ipa_pure_const =
+{
+  true, /* has_generate_summary */
+  true, /* has_write_summary */
+  true, /* has_read_summary */
+  false, /* has_write_optimization_summary */
+  false, /* has_read_optimization_summary */
+  false, /* has_stmt_fixup */
+  0, /* function_transform_todo_flags_start */
+  false, /* has_function_transform */
+  false /* has_variable_transform */
+};
+
+class pass_ipa_pure_const : public ipa_opt_pass_d
+{
+public:
+  pass_ipa_pure_const(gcc::context *ctxt)
+    : ipa_opt_pass_d(pass_data_ipa_pure_const, ctxt,
+\t\t     ipa_pass_data_ipa_pure_const)
+  {}
+
+  /* opt_pass methods: */
+  bool gate () { return gate_pure_const (); }
+  unsigned int execute () { return propagate (); }
+
+  /* ipa_opt_pass_d methods: */
+  void generate_summary (void)
+  {
+    pure_const_generate_summary ();
+  }
+
+  void write_summary (void)
+  {
+    pure_const_write_summary ();
+  }
+
+  void read_summary (void)
+  {
+    pure_const_read_summary ();
+  }
+
+}; // class pass_ipa_pure_const
+
+} // anon namespace
+""")
+        expected_changelog = \
+            ('\t* ipa-pure-const.c (pass_ipa_pure_const): Convert to new API for IPA\n'
+             '\tpasses.\n')
+
+        self.assertRefactoringEquals(src, 'ipa-pure-const.c',
                                      expected_code, expected_changelog)
 
 if __name__ == '__main__':
