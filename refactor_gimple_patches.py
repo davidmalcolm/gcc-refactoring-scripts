30fd322b1de596dc2b8a8f05b590b8f96f42c1e3
David Malcolm <dmalcolm@redhat.com>
2014-10-23 16:54:19 -0400

    Work towards renaming subclasses directly in patches

--- refactor_gimple_patches.py
+++ refactor_gimple_patches.py
@@ -1,3 +1,4 @@
+from collections import namedtuple
 from email.parser import Parser
 import glob
 import os
@@ -6,6 +7,7 @@ import textwrap
 
 from gcc_mail_archive import MailArchive
 from gimple_approvals import APPROVALS
+from refactor import Source, not_identifier
 
 INDEX_URL = 'https://gcc.gnu.org/ml/gcc-patches/2014-04/index.html'
 IN_DIR = '../src/v11-patches'
@@ -37,6 +39,152 @@ class Patch:
         summary = summary.replace('\n', '')
         return summary
 
+class StmtClass(namedtuple('StmtClass',
+                           ('orig_name', 'typedef', 'new_name'))):
+    pass
+
+stmt_classes = [
+    StmtClass('gimple_statement_cond',
+              'gimple_cond',
+              'gcond'),
+    StmtClass('gimple_statement_debug',
+              'gimple_debug',
+              'gdebug'),
+    StmtClass('gimple_statement_goto',
+              'gimple_goto',
+              'ggoto'),
+    StmtClass('gimple_statement_label',
+              'gimple_label',
+              'glabel'),
+    StmtClass('gimple_statement_switch',
+              'gimple_switch',
+              'gswitch'),
+    StmtClass('gimple_statement_assign',
+              'gimple_assign',
+              'gassign'),
+    StmtClass('gimple_statement_asm',
+              'gimple_asm',
+              'gasm'),
+    StmtClass('gimple_statement_call',
+              'gimple_call',
+              'gcall'),
+    StmtClass('gimple_statement_transaction',
+              'gimple_transaction',
+              'gtransaction'),
+    StmtClass('gimple_statement_return',
+              'gimple_return',
+              'greturn'),
+    StmtClass('gimple_statement_bind',
+              'gimple_bind',
+              'gbind'),
+    StmtClass('gimple_statement_catch',
+              'gimple_catch',
+              'gcatch'),
+    StmtClass('gimple_statement_eh_filter',
+              'gimple_eh_filter',
+              'geh_filter'),
+    StmtClass('gimple_statement_eh_mnt',
+              'gimple_eh_must_not_throw',
+              'geh_mnt'),
+    StmtClass('gimple_statement_eh_else',
+              'gimple_eh_else',
+              'geh_else'),
+    StmtClass('gimple_statement_resx',
+              'gimple_resx',
+              'gresx'),
+    StmtClass('gimple_statement_eh_dispatch',
+              'gimple_eh_dispatch',
+              'geh_dispatch'),
+    StmtClass('gimple_statement_phi',
+              'gimple_phi',
+              'gphi'),
+    StmtClass('gimple_statement_try',
+              'gimple_try',
+              'gtry'),
+    StmtClass('gimple_statement_omp_atomic_load',
+              'gimple_omp_atomic_load',
+              'gomp_atomic_load'),
+    StmtClass('gimple_statement_omp_atomic_store',
+              'gimple_omp_atomic_store',
+              'gomp_atomic_store'),
+    StmtClass('gimple_statement_omp_continue',
+              'gimple_omp_continue',
+              'gomp_continue'),
+    StmtClass('gimple_statement_omp_critical',
+              'gimple_omp_critical',
+              'gomp_critical'),
+    StmtClass('gimple_statement_omp_for',
+              'gimple_omp_for',
+              'gomp_for'),
+    StmtClass('gimple_statement_omp_parallel',
+              'gimple_omp_parallel',
+              'gomp_parallel'),
+    StmtClass('gimple_statement_omp_task',
+              'gimple_omp_task',
+              'gomp_task'),
+    StmtClass('gimple_statement_omp_sections',
+              'gimple_omp_sections',
+              'gomp_sections'),
+    StmtClass('gimple_statement_omp_single',
+              'gimple_omp_single',
+              'gomp_single'),
+    StmtClass('gimple_statement_omp_target',
+              'gimple_omp_target',
+              'gomp_target'),
+    StmtClass('gimple_statement_omp_teams',
+              'gimple_omp_teams',
+              'gomp_teams'),
+]
+
+print(stmt_classes)
+
+def rename_types(text):
+    """
+    Update patches to follow the naming convention from
+      https://gcc.gnu.org/ml/gcc-patches/2014-05/msg00346.html
+
+    Rename new types:
+      "ORIG_NAME" -> "NEW_NAME"
+      "TYPEDEF" -> "NEW_NAME *"
+      "const_TYPEDEF" -> "const NEW_NAME *"
+    e.g.
+      "gimple_statement_switch" -> "gswitch"
+      "gimple_switch" -> "gswitch *"
+      "const_gimple_switch" -> "const gswitch *"
+    FIXME: Only touch lines in ChangeLog, and those beginning with a +???
+    FIXME: but we need to touch the - as well sometimes
+    """
+    src = Source(text)
+    for subclass in stmt_classes:
+        for old, new in ((subclass.orig_name,
+                          subclass.new_name),
+                         (subclass.typedef,
+                          '%s *' % subclass.new_name),
+                         ('const_%s' % subclass.typedef,
+                          'const %s *' % subclass.new_name)):
+            # this works backwards through the file
+            for m in src.finditer(not_identifier + ('(%s)' % old) + not_identifier):
+                if 0:
+                    print(m.start(1))
+                    print(m.end(1))
+                    print(src._str[m.start(1):m.end(1)])
+                replacement = new
+                start, end = m.start(1), m.end(1)
+
+                # Avoid turning:
+                #   gimple_switch stmt
+                # into
+                #   gswitch * stmt
+                # converting into
+                #   gswitch *stmt
+                # instead.
+                if new.endswith(' *') and src._str[end] == ' ':
+                    end += 1
+                # FIXME: though not in a ChangeLog fragment
+
+                src = src.replace(start, end, replacement)
+    return src.str()
+
 def main():
     # Read mail archive
     ma = MailArchive('gcc-patches-2014-04-index.html')
@@ -51,6 +199,10 @@ def main():
         summary = p.summary
         if 0:
             print(repr(summary))
+
+        text = str(p.msg.get_payload())
+        text = rename_types(text)
+        p.msg.set_payload(text)
 
         # Locate the corresponding patch submitted in April 2014:
         pat = r'\[PATCH ([0-9]+)/89\] %s' % summary
